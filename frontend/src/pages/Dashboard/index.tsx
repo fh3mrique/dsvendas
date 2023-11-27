8f01a9088373a6031d3fa318b0ba5c84e347befa
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-27 16:20:35 -0300

    Rotas

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,37 @@
+import BarCharts from "../../components/BarCharts";
+import DataTable from "../../components/DataTable";
+import DonutChart from "../../components/DonutCharts";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de sucesso (%)</h5>
+            <BarCharts />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de sucesso (%)</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
