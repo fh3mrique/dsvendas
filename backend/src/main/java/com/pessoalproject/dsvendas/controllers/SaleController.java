2d296c4295ded593e42c4381357ede601b8f60fc
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-27 17:31:40 -0300

    Primeira requisição dos dados de DonutChart

--- backend/src/main/java/com/pessoalproject/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/pessoalproject/dsvendas/controllers/SaleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import com.pessoalproject.dsvendas.services.SaleService;
 
 @RestController
 @RequestMapping(value = "sales")
+@CrossOrigin(origins = "*")
 public class SaleController {
 	
 	@Autowired
--- frontend/src/components/DonutCharts/index.tsx
+++ frontend/src/components/DonutCharts/index.tsx
@@ -1,12 +1,38 @@
+import axios from "axios";
 import Chart from "react-apexcharts"
+import { BASE_URL } from "../../utils/request";
+import { SaleSum } from "../../types/sales";
+
+//declarando tipo
+type ChartData = {
+    labels: string[]
+    series: number[];
+}
+
 
 const DonutChart = () => {
+
+    //instanciando vazio
+    let chartData : ChartData = {labels: [], series: []}
+
+    axios.get(`${BASE_URL}sales/sum-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSum[];
+
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum)
+
+            chartData = {labels: myLabels, series: mySeries};
+            console.log(chartData)
+
+            console.log(chartData)
+        })
   
-    const mockData = {
+   /*  const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+     */
     const options = {
         legend: {
             show: true
@@ -15,8 +41,8 @@ const DonutChart = () => {
 
   return (
     <Chart
-        options={{ ...options, labels: mockData.labels}}
-        series={mockData.series}
+        options={{ ...options, labels: chartData.labels}}
+        series={chartData.series}
         type="donut"
         height="240"
     
